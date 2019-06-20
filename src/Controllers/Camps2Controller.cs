499a8630ac738d00331497d20fa52f5622ff03d3
qishichang <qishichang@live.com>
2019-06-21 07:12:33 +1200

    Add Api version support

--- src/Controllers/Camps2Controller.cs
+++ src/Controllers/Camps2Controller.cs
@@ -11,16 +11,15 @@ using System.Threading.Tasks;
 
 namespace CoreCodeCamp.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiVersion("1.0")]
-    [ApiVersion("1.1")]
+    [Route("api/camps")]
+    [ApiVersion("2.0")]
     [ApiController]
-    public class CampsController : ControllerBase
+    public class Camps2Controller : ControllerBase
     {
         private readonly ICampRepository _repository;
         private readonly IMapper _mapper;
 
-        public CampsController(ICampRepository repository,
+        public Camps2Controller(ICampRepository repository,
             IMapper mapper)
         {
             _repository = repository;
@@ -28,12 +27,17 @@ namespace CoreCodeCamp.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<CampModel[]>> GetCamps(bool includeTalks = false)
+        public async Task<IActionResult> GetCamps(bool includeTalks = false)
         {
             try
             {
                 var results = await _repository.GetAllCampsAsync(includeTalks);
-                return _mapper.Map<CampModel[]>(results);
+                var result = new
+                {
+                    Results = _mapper.Map<CampModel[]>(results),
+                    Count = results.Count()
+                };
+                return Ok(result);
             }
             catch (Exception)
             {
@@ -42,28 +46,11 @@ namespace CoreCodeCamp.Controllers
         }
 
         [HttpGet("{moniker}")]
-        [MapToApiVersion("1.0")]
         public async Task<ActionResult<CampModel>> GetCampByMoniker(string moniker)
         {
             try
             {
                 var result = await _repository.GetCampAsync(moniker);
-                if (result == null) return NotFound();
-                return _mapper.Map<CampModel>(result);
-            }
-            catch (Exception)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
-            }
-        }
-
-        [HttpGet("{moniker}")]
-        [MapToApiVersion("1.1")]
-        public async Task<ActionResult<CampModel>> GetCampByMoniker11(string moniker)
-        {
-            try
-            {
-                var result = await _repository.GetCampAsync(moniker, true);
                 if (result == null) return NotFound();
                 return _mapper.Map<CampModel>(result);
             }
--- src/Startup.cs
+++ src/Startup.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using CoreCodeCamp.Controllers;
 using CoreCodeCamp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc.Versioning.Conventions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -23,6 +26,22 @@ namespace CoreCodeCamp
             services.AddScoped<ICampRepository, CampRepository>();
 
             services.AddAutoMapper(typeof(Startup));
+
+            services.AddApiVersioning(opt =>
+            {
+                opt.AssumeDefaultVersionWhenUnspecified = true;
+                opt.DefaultApiVersion = new ApiVersion(1, 1);
+                opt.ReportApiVersions = true;
+                opt.ApiVersionReader = ApiVersionReader.Combine(
+                    new HeaderApiVersionReader("X-Version"),
+                    new QueryStringApiVersionReader("v", "ver", "version"));
+
+                opt.Conventions.Controller<TalksController>()
+                    .HasApiVersion(new ApiVersion(1, 0))
+                    .HasApiVersion(new ApiVersion(1, 1))
+                    .Action(c => c.DeleteTalk(default(string), default(int)))
+                        .MapToApiVersion(new ApiVersion(1, 1));
+            });
 
             services.AddMvc()
               .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
